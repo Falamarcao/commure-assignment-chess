36c819ebe3a299165e569d738d59523972777636
falamarcao <falamarcao@gmail.com>
2024-10-19 15:24:50 -0300

    improvements

--- commure_assignment_chess/main.py
+++ commure_assignment_chess/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from typing import List, Union
+
 import csv
 from datetime import datetime, timedelta
 from json import dumps as json_dumps
@@ -13,42 +15,30 @@ class Assignment:
 
     def __init__(self):
         self.today = datetime.today()
-
-        self.months = {
-            0: "Jan",
-            1: "Feb",
-            2: "Mar",
-            3: "Apr",
-            4: "May",
-            5: "Jun",
-            6: "Jul",
-            7: "Aug",
-            8: "Sep",
-            9: "Oct",
-            10: "Nov",
-            11: "Dec",
-        }
-
         self.lichess = Lichess()
 
-    def is_within_last_30_days(self, check_date):
+    def is_within_last_30_days(self, check_date) -> bool:
         # Calculate the date 30 days ago
         cutoff_date = self.today - timedelta(days=30)
 
         # Check if the check_date is between cutoff_date and today
         return cutoff_date <= check_date <= self.today
 
-    def list_50_top_classical_chess_players_usernames(self):
-        # 1. List the top 50 classical chess players. Just print their usernames.
+    def list_n_top_classical_chess_players_usernames(self, n: int) -> List[str]:
+        # 1. List the top n classical chess players. Just print their usernames.
         usernames = []
-        for user in self.lichess.get_one_leaderboard(nb=50, perfType="classical")[
+        for user in self.lichess.get_one_leaderboard(nb=n, perfType="classical")[
             "users"
         ]:
             usernames.append(user["username"])
 
         return usernames
 
-    def rating_history_chess_player(self, username: str, complete_date: bool = False):
+    def rating_history_chess_player(
+        self, username: str, complete_date: bool = False, returnDataFrame: bool = False
+    ) -> Union[dict, pd.DataFrame]:
+        date_format = "%Y-%m-%d" if complete_date else "%b %d"
+
         rating_history = self.lichess.get_rating_history_of_a_user(username=username)
 
         data = dict()
@@ -57,16 +47,9 @@ class Assignment:
                 for record in reversed(game["points"]):
                     date = datetime(record[0], record[1] + 1, record[2])
                     if self.is_within_last_30_days(date):
-                        if complete_date:
-                            data.update({date.strftime("%Y-%m-%d"): record[3]})
-                        else:
-                            data.update(
-                                {f"{self.months[record[1]]} {record[2]}": record[3]}
-                            )
+                        data.update({date.strftime(date_format): record[3]})
                     else:
                         break
-
-        date_format = "%Y-%m-%d" if complete_date else "%b %d"
 
         # Convert the dictionary to a DataFrame
         df = pd.DataFrame(list(data.items()), columns=["Date", "Value"])
@@ -82,71 +65,49 @@ class Assignment:
         df = df.reindex(date_range)
 
         # Fill missing values with the last available date's value
-        df["Value"] = (
-            df["Value"].ffill().bfill()
-        )  # Use ffill and bfill directly on the Series
+        df["Value"] = df["Value"].ffill().bfill().astype(int)
 
-        # Convert the index (Timestamps) back to strings for dict compatibility
-        return {
-            date.strftime(date_format): int(value)
-            for date, value in df["Value"].to_dict().items()
-        }
+        df.index = df.index.strftime(date_format)
 
-    def rating_history_n_players_30_days_to_csv(self, number_of_players: int):
+        if returnDataFrame:
+            df = df.transpose()
+            df.insert(0, "username", username)  # insert collumn username
+            return df
+        else:
+            return df.to_dict()["Value"]
+
+    def rating_history_n_players_30_days_to_csv(self, number_of_players: int) -> None:
         players = self.lichess.get_one_leaderboard(
             nb=number_of_players, perfType="classical"
         )["users"]
 
-        data = []
-        for player in players:
-            data.append(
-                {
-                    player["username"]: self.rating_history_chess_player(
-                        player["username"], complete_date=True
-                    )
-                }
+        player_dfs = [
+            self.rating_history_chess_player(
+                username=player["username"], complete_date=True, returnDataFrame=True
             )
+            for player in players
+        ]
 
-        # Extract the first user's dates (since they are the same and in order)
-        dates = list(data[0][list(data[0].keys())[0]].keys())
+        df = pd.concat(player_dfs)
 
-        # Prepare the data for CSV
-        csv_data = []
-
-        for entry in data:
-            for username, values in entry.items():
-                row = [username]  # Start the row with the username
-                row.extend(
-                    [values.get(date, 'N/A') for date in dates]
-                )  # Add the values in date order
-                csv_data.append(row)
-
-        # Define the CSV file path
-        csv_file_path = f"rating_history_{number_of_players}_players_30_days.csv"
-
-        # Write to CSV
-        with open(csv_file_path, mode="w", newline="") as file:
-            writer = csv.writer(file)
-            # Write the header: "Username" followed by the dates
-            writer.writerow(["Username"] + dates)
-            # Write the rows: username followed by values for each date
-            writer.writerows(csv_data)
+        df.to_csv(f"rating_history_{number_of_players}_players_30_days.csv", index=False)
 
 
 if __name__ == "__main__":
     assignment = Assignment()
 
-    print(assignment.list_50_top_classical_chess_players_usernames())
+    print(assignment.list_n_top_classical_chess_players_usernames(n=50))
 
     player = assignment.lichess.get_one_leaderboard(nb=1, perfType="classical")[
         "users"
     ][0]
 
-    print(f'- {player["username"]}, ', 
+    print(
+        f'- {player["username"]}, ',
         json_dumps(
             assignment.rating_history_chess_player(username=player["username"]),
             indent=4,
-        )
+        ),
     )
 
     assignment.rating_history_n_players_30_days_to_csv(number_of_players=50)
